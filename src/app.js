f55d4c5ce84b7ff9d884f8b195f7f687588b541a
V-Emanuel <gvictor567@gmail.com>
2023-01-13 02:17:08 -0300

    connecting database using dotenv

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 
 const PORT = 5000;
 const app = express();
@@ -8,4 +10,17 @@ app.use(cors());
 app.use(express.json());
 app.listen(PORT);
 
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+let db;
+
+mongoClient.connect()
+
+.then(() => {
+	db = mongoClient.db();
+    console.log("Deu Certo")
+})
+
+.catch(()=>{
+    console.log("Deu errado")
+})
 
