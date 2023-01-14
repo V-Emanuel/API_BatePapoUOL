fbba8e83948147ab133b7b9ef2a7c80bfb6bbd96
V-Emanuel <gvictor567@gmail.com>
2023-01-14 00:05:04 -0300

    create post and get '/participants'

--- src/app.js
+++ src/app.js
@@ -14,13 +14,34 @@ const mongoClient = new MongoClient(process.env.DATABASE_URL);
 let db;
 
 mongoClient.connect()
-
 .then(() => {
 	db = mongoClient.db();
-    console.log("Deu Certo")
 })
-
-.catch(()=>{
+.catch((err)=>{
+    console.log(err)
     console.log("Deu errado")
 })
 
+app.post('/participants', async (req, res) => {
+    const {name} = req.body;
+    const lastStatus = Date.now()
+    try{
+        const nameExist = await db.collection('participants').findOne({name})
+        if (nameExist) return res.status(409).send("esse usuário já existe")
+        await db.collection('participants').insertOne({name, lastStatus})
+        res.send('Ok')
+    }catch(err){
+        console.log(err)
+        res.status(500).send("Deu algo errado no servidor")
+    } 
+})
+
+app.get('/participants', async (res, res) => {
+    try {
+        const users = await db.collection("participants").find().toArray()
+        if (!users) return res.status(404).send("Não há participantes")
+        res.send(users)
+      } catch (error) {
+        res.status(500).send("Deu um erro no servidor de banco de dados")
+      }
+})
