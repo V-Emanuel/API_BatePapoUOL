123cfae64d6764a5d6e15490c492e444e5b8ea33
V-Emanuel <gvictor567@gmail.com>
2023-01-16 01:38:57 -0300

    correcting post /messages

--- src/app.js
+++ src/app.js
@@ -27,7 +27,6 @@ try {
 
 app.post('/participants', async (req, res) => {
     const user = req.body;
-    const lastStatus = Date.now();
     const userSchema = joi.object({
         name: joi.string().required()
     });
@@ -67,28 +66,28 @@ app.get('/participants', async (req, res) => {
 })
 
 app.post('/messages', async (req, res) => {
-    const { to, text, type} = req.body;
+    const messages = req.body;
+    const user = req.headers.user
     const messageSchema = joi.object({
         to: joi.string().required(),
         text: joi.string().required(),
         type: joi.string().required()
     });
-    const validation = messageSchema.validate({to, text, type}, { abortEarly: true });
+    const validation = messageSchema.validate(messages, { abortEarly: true });
     if (validation.error) {
         const errors = validation.error.details.map((detail) => detail.message);
         return res.status(422).send(errors);
     }
     try {
         await db.collection("messages").insertOne({
-            to,
-            text,
-            type,
+            from: user, 
+            ...messages,
             time: dayjs().format('HH:mm:ss')
         })
         res.send('Ok')
     } catch (err) {
         console.log(err)
-        res.status(500).send("Deu algo errado no servidor")
+        return res.sendStatus(500).send(err.message)
     }
 })
 
